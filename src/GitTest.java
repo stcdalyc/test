f562d3121a8dd41f1aaaa7788134c01ccc41a109
stcdalyc <stcdalyc@gmail.com>
2014-02-23 23:55:05 +0800

    4.0

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,7 +6,8 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("3.0");
+        System.out.println("4.0");
+        
 	}
 
 }
