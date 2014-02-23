c28077820ffa4761e15f8ab91cad3dc4499bde34
stcdalyc <stcdalyc@gmail.com>
2014-02-23 23:57:13 +0800

    v9.0

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("8.0");
+        System.out.println("9.0");
 	}
 
 }
